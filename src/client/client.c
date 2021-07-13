de16c8b7d751cc44c0a887e9fdfbdbc206e062a5
3lswear <r.malyavsky@protonmail.com>
2021-07-13 20:28:14 +0300

    added usage output

--- src/client/client.c
+++ src/client/client.c
@@ -3,30 +3,28 @@
 void client_handler(int signum)
 {
 	if (signum == SIGUSR2)
-	{
 		ft_putendl_fd("[i] Message was received.", 1);
-	}
 	else
 		usleep(1);
 }
 
 void	send_byte(char byte, int pid)
 {
-	int i = 7;
-	short err;
+	int i;
+	int err;
 
+	i = 8;
 	err = 0;
-	while (i >= 0)
+	while (i-- >= 1)
 	{
 		if ((byte >> i) & 1)
 			err = kill(pid, SIGUSR2);
 		else
 			err = kill(pid, SIGUSR1);
 		usleep(40000);
-		i--;
 		if (err)
 		{
-			ft_putendl_fd("[x] Server died :(", 2);
+			ft_putendl_fd("[x] Server is dead :(", 2);
 			exit(-1);
 		}
 	}
@@ -39,6 +37,7 @@ void	send_str(char *str, int pid)
 
 	len = ft_strlen(str);
 	i = 0;
+	printf("len is %u\n", len);
 
 	while (i < 4)
 	{
@@ -62,22 +61,18 @@ int		main(int argc ,char **argv)
 		pid = ft_atoi(argv[1]);
 		str = argv[2];
 	}
-	else
+	else 
+	{
+		ft_putendl_fd("Usage: client PID STRING", 2);
 		return(-1);
-
-	printf("pid is %d\n", pid);
+	}
 
 	struct sigaction client_action;
-
 	client_action.sa_handler = client_handler;
 	sigemptyset(&client_action.sa_mask);
-	/* sigaddset(&client_action.sa_mask, SIGUSR1); */
 	client_action.sa_flags = 0;
-
 	sigaction(SIGUSR1, &client_action, NULL);
 	sigaction(SIGUSR2, &client_action, NULL);
-
 	send_str(str, pid);
-
 	return (0);
 }
