8bece518f2455bb733c40c73d364454d01bb89ce
3lswear <r.malyavsky@protonmail.com>
2021-07-14 18:31:03 +0300

    normed

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -1,14 +1,14 @@
 #ifndef MINITALK_H
-#define MINITALK_H
+# define MINITALK_H
 
-#include "../libft/libft.h"
-#include <unistd.h>
-#include <signal.h>
-#include <stdlib.h>
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
 
-#include <stdio.h>
-
+# include <stdio.h>
 
 void	send_byte(char byte, int pid);
+void	print_err(int code, char *msg);
 
 #endif
