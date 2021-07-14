fa40cc876a0e2a670f81a151abf3081e1ffe79c8
3lswear <r.malyavsky@protonmail.com>
2021-07-14 18:59:21 +0300

    some experimental refactoring

--- src/server/server.c
+++ src/server/server.c
@@ -1,59 +1,72 @@
 #include "minitalk.h"
 
-/* void	save_byte(char *c, char *str, size_t *i, unsigned int *len) */
-/* { */
+/* void	put_char() */
 
-
-/* } */
-
-void end_transfer(char *str, size_t *i, unsigned int *len, pid_t pid)
+void	recv_len(int bitn, char c, char **str)
 {
-	printf("len is %u\n", *len);
-	ft_putstr_fd(str, 1);
-	ft_putendl_fd("", 1);
-	free(str);
-	/* exit(0); */
-	*i = 0;
-	*len = 0;
-	kill(pid, SIGUSR2);
+	static unsigned int	len;
+
+	len = (len << 8) | (c & 0xFF);
+	if (bitn == 32)
+	{
+		printf("len is %u\n", len);
+		ft_putendl_fd("allocing", 1);
+		*str = ft_calloc(len + 1, sizeof(char));
+		len = 0;
+	}
+
 }
 
-void sig_handler(int signum, siginfo_t *info, void *ucontext)
+void	end_transfer(char **str, size_t *bitn, pid_t pid)
 {
-	static size_t i = 0; // received bit number
-	static char c = 0; //assembled byte
-	static unsigned int len = 0; // string length
-	static char *str; // assembled string
-	(void)ucontext;
+	int	err;
 
-	i++;
+	err = 0;
+	ft_putstr_fd(*str, 1);
+	ft_putendl_fd("", 1);
+	free(*str);
+	/* exit(0); */
+	*bitn = 0;
+	err = kill(pid, SIGUSR2);
+	if (err)
+		ft_putendl_fd("[w] Client didn\'t receive acknowledgment :(", 2);
+}
+
+void	sig_handler(int signum, siginfo_t *info, void *ucontext)
+{
+	static size_t	bitn = 0; // received bit number
+	static char		c = 0; //assembled byte
+	static char 	*str; // assembled string
+	/* int 			err; */
+
+	(void)ucontext;
+	/* err = 0; */
+	bitn++;
 	c = c << 1;
 	if (signum == SIGUSR2)
 		c = c | 1;
-	if (i % 8 == 0) //read a byte
+	if (bitn % 8 == 0) //read a byte
 	{
-		if (i <= 32) // receiving len
-		{
-			len = (len << 8) | (c & 0xFF);
-			if (i == 32)
-				str = ft_calloc(len + 1, sizeof(char));
-		}
+		if (bitn <= 32) // receiving len
+			recv_len(bitn, c, &str);
 		else
 		{
 			if (str)
-				str[(i - 40) / 8] = c;
+				str[(bitn - 40) / 8] = c;
 			else
-			{
-				ft_putendl_fd("\n[x] malloc error", 2);
-				exit(-1);
-			}
+				print_err(-1, "malloc error");
 			/* ft_putchar_fd(c, 1); */
 			if (!c) //if transfer ended
-				end_transfer(str, &i, &len, info->si_pid);
+				end_transfer(&str, &bitn, info->si_pid);
 		}
 		c = 0;
 	}
 	kill(info->si_pid, SIGUSR1);
+	/* if (err) */
+	/* { */
+	/* 	ft_putendl_fd("[x] Client is dead", 2); */
+	/* 	end_transfer(&str, &bitn, info->si_pid); */
+	/* } */
 }
 
 int main(void)
@@ -69,18 +82,15 @@ int main(void)
 
 	new_action.sa_sigaction = sig_handler;
 	sigemptyset(&new_action.sa_mask);
-	sigaddset(&new_action.sa_mask, SIGUSR1);
-	sigaddset(&new_action.sa_mask, SIGUSR2);
+	/* sigaddset(&new_action.sa_mask, SIGUSR1); */
+	/* sigaddset(&new_action.sa_mask, SIGUSR2); */
 	new_action.sa_flags = SA_SIGINFO;
-
 	sigaction(SIGUSR1, &new_action, NULL);
 	sigaction(SIGUSR2, &new_action, NULL);
-
-	/* printf("%s", art); */
-	/* printf("[i] My pid is: %d\n", getpid()); */
-
+	ft_putstr_fd("[i] My pid is: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putendl_fd("", 1);
 	while(1)
-		/* sleep(1); */
 		pause();
 
 	return (0);
